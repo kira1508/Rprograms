14da8423df13b0f20455eaca33f231bd17c7a389
kira1508 <kiranhebbar96@gmail.com>
2017-12-04 16:06:25 +0530

    added 4thR program

---
+++ tra4.R
@@ -0,0 +1,21 @@
+
+library(rpart)
+library(datasets)
+library(e1071)
+str(iris)
+ind <- sample(2, nrow(iris),replace = TRUE, prob = c(0.5,0.5))
+trainData <- iris[ind ==1,]
+testData <- iris[ind ==2,]
+my_plot <- rpart( Species ~ Sepal.Length + Sepal.Width + Petal.Length + Petal.Width ,trainData,
+                  method = "class")
+summary(my_plot)
+rpart.plot::rpart.plot(my_plot)
+res <- predict(my_plot,testData)
+table(res,testData$Species)
+summary(res)
+print(res)
+pred <- naiveBayes(Species ~.,data = trainData,laplace = 1)
+#mosaicplot(pred)
+ans <- predict(pred,testData)
+table(ans,testData$Species)
+summary(ans)
