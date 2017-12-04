f56b90326a08b0a027a654c377ecf354809b9cb6
kira1508 <kiranhebbar96@gmail.com>
2017-12-04 16:07:18 +0530

    added kmeans

---
+++ kmeansR.R
@@ -0,0 +1,10 @@
+library(datasets)
+library(cluster)
+library(factoextra)
+lindata <- iris
+lindata$Species<-NULL 
+d<- scale(dist(lindata,method = "euclidian"))
+kfit <- kmeans(d,3)
+hfit <- hkmeans(d,3)
+fviz_cluster(hfit, lindata, geom = "point")
+fviz_cluster(kfit, lindata, geom = "point")
